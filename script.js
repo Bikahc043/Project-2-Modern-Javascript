b93ce8fd3f2a2f3ad92a2ef114574ecd90325900
Bikash chetia <146708139+Bikahc043@users.noreply.github.com>
2023-12-20 17:08:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let x="red";
+//string
+let string_demo="This is a string";
+console.log(string_demo);
 
-console.log(x);
-function change_to_blue(){
-    let inside_x ="blue";
-    console.log("inside function data:"+ inside_x);
-    }
-change_to_blue()  
-console.log(x);
+//number
+let number_demo="This is a string";
+console.log(number_demo);
+
+let floats_demo="This is a string";
+console.log(floats_demo);
+
+let booleans_demo="This is a string";
+console.log(booleans_demo);
+
+let null_demo="This is a string";
+console.log(null_demo);
